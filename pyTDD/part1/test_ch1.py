3552230c001aaf05d5a2589abbefed45949cfff6
jjmachan <jamesjithin97@gmail.com>
2023-10-13 22:33:36 +0530

    added TDD

--- pyTDD/part1/test_ch1.py
+++ pyTDD/part1/test_ch1.py
@@ -1,17 +1,45 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 
-@dataclass
-class Dollar:
-    amount: float
+class Money:
+    def __init__(self, amount):
+        self.amount = amount
 
-    def times(self, x):
+    def __eq__(self, x: Money) -> bool:
+        return self.amount == x.amount and type(self) == type(x)
+
+
+class Dollar(Money):
+    def __mul__(self, x):
+        # Value Objects
         return Dollar(self.amount * x)
+
+
+class Franc(Money):
+    def __mul__(self, x):
+        # Value Objects
+        return Franc(self.amount * x)
 
 
 def test_multiplication():
     five = Dollar(5)
-    product = five.times(2)
-    assert product.amount == 10
-    product = five.times(3)
-    assert product.amount == 15
+    assert five * 2 == Dollar(10)
+    assert five * 3 == Dollar(15)
+
+
+def test_equity():
+    assert Dollar(5) == Dollar(5)
+    assert not Dollar(5) == Dollar(6)
+
+    assert Franc(5) == Franc(5)
+    assert not Franc(5) == Franc(6)
+
+    assert Franc(5) != Dollar(5)
+
+
+def test_franc_multiplication():
+    five = Franc(5)
+    assert Franc(10) == five * 2
+    assert Franc(15) == five * 3
